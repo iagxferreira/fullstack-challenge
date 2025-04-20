245792561ceefa22898b8a7829b66e22dc332b32
Iago Ferreira <iago-ferreira@outlook.com>
2025-04-19 23:41:03 -0300

    feat: added swagger support

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fullstack challenge.')
+    .setDescription('The fullstack challenge API.')
+    .setVersion('1.0')
+    .addTag('challenge')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(process.env.MONGODB_URI);
 }
 bootstrap();
